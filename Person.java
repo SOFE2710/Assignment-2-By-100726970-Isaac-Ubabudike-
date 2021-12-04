f72aafcd1ca94a0c709296425fbd4f74ba5afe69
isaacubabudike <94243758+isaacubabudike@users.noreply.github.com>
2021-12-03 22:26:05 -0500

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,27 +1,27 @@
 public class Person {
-   private String   name;
-   
-   
-   public Person(String initialName)
-   {
-      
+   protected String name;
+
+   // constructors
+   public Person(String initialName){
+      name = initialName;
    }
-   
-   public Person()
-   {
-     
+   public Person(){
    }
-   
    public void setName( String fullName) {
-
-    }
-
-   public String getName() {
-      return null;
+      name = fullName;
    }
- 
+   public String getName() {
+      return name;
+   }
+   // this method is necessary because in TestApp.java, Student object is assigned to Parent object
+   // and then these methods are called
+   public void registerFor(Course course) {
+   }
+   // same reason as previous one
+   public boolean isRegisteredInCourse(Course course) {
+      return false;
+   }
    public String toString() {
-   return null;
-
+      return "Person class; Name: " + name;
    }
 }
